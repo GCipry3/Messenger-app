a776106d8360b704c5ecaa9a8d98802d29222f8e
GCipry3 <galbeazaciprian30@gmail.com>
2023-01-19 01:38:56 +0200

    Connect to database, Added flash messages into the layout

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session , flash
 from wtform_fields import *
+from models import *
 
+# Configure app
 app = Flask(__name__)
 app.secret_key = 'super secret key'
+
+
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://GCipry3:RsjHGvmh40DM@ep-square-math-535766.eu-central-1.aws.neon.tech/neondb'
+db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,6 +18,19 @@ def index():
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
+
+        # Check username
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            flash('This username is already taken.')
+            return redirect(url_for('index'))
+        
+        # Add user to database
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        
+        flash('You have successfully registered.')
         return redirect(url_for('index'))
 
     return render_template('index.html',form = reg_form)
