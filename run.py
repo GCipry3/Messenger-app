941f6eaf9a7fa897a55c00137495e5102f34b48d
GCipry3 <galbeazaciprian30@gmail.com>
2023-01-19 02:03:10 +0200

    Create login page and login validation

--- run.py
+++ run.py
@@ -19,22 +19,31 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
-        # Check username
-        user_object = User.query.filter_by(username=username).first()
-        if user_object:
-            flash('This username is already taken.')
-            return redirect(url_for('index'))
-        
         # Add user to database
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        
+
         flash('You have successfully registered.')
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
 
     return render_template('index.html',form = reg_form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        
+        if user_object and user_object.password == login_form.password.data:
+            flash('Logged in successfully.')
+            return redirect(url_for('login'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+    
+    return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
